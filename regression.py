b7a5d833341a60c0fc071e0b223f7d0586c194c3
Antonin Delpeuch <antonin@delpeuch.eu>
2014-05-07 17:43:43 +0100

    Added plots to watch the variation of the cost

--- regression.py
+++ regression.py
@@ -2,6 +2,13 @@
 import numpy as np
 import math
 
+def frobenius_norm_squared(A):
+    A2 = np.multiply(A,A)
+    return np.real(np.sum(A2))
+
+def fitness(inputs, outputs, A):
+    return frobenius_norm_squared(np.dot(inputs, A) - outputs)
+
 def ridge(inputs, outputs, lmbd):
     """Implements ridge regression using the closed formula"""
     # Computes min_A || inputs * A - outputs ||^2 + lmbd * || A ||^2
@@ -15,7 +22,10 @@ def ridge(inputs, outputs, lmbd):
     inputst = np.transpose(inputs)
     A = np.dot(np.dot(np.linalg.pinv(np.dot(inputst, inputs) + lmbdid), inputst), outputs)
 
-    return A
+    cost = [fitness(inputs, outputs, A), lmbd*frobenius_norm_squared(A)]
+
+    return A, cost
+
 
 def incremental_ridge(inputs, outputs, lmbd, iterations, stepsize):
     """Implements ridge regression using gradient descent"""
@@ -35,19 +45,20 @@ def incremental_ridge(inputs, outputs, lmbd, iterations, stepsize):
     utuid = utu + lmbdid
     uv = np.dot(inputst, outputs)
 
+    costs = []
+
     for i in range(iterations):
         # TODO: How to choose the step size?
+        costs.append([frobenius_norm_squared(np.dot(inputs,A) - outputs),
+                      lmbd* frobenius_norm_squared(A)])
         gradient = np.dot(utuid,A) - uv
         A = A - stepsize * gradient
 
-    return A
+    return A, costs
 
-def frobenius_norm_squared(A):
-    A2 = np.multiply(A,A)
-    return np.real(np.sum(A2))
-
-def fitness(inputs, outputs, A):
-    return frobenius_norm_squared(np.dot(inputs, A) - outputs)
+def tracenorm(A):
+    U, s, V = np.linalg.svd(A)
+    return sum(s)
 
 def incremental_tracenorm(inputs, outputs, lmbd, iterations, stepsize):
     """ Implements trace norm regression using the first gradient descent from (Ji and Ye, 2009)"""
@@ -65,25 +76,51 @@ def incremental_tracenorm(inputs, outputs, lmbd, iterations, stepsize):
     #unused:  utuid = utu + lmbdid
     uv = np.dot(inputst, outputs)
 
+    costs = []
+
     for i in range(iterations):
         # TODO: How to choose the step size?
+
+        costs.append([frobenius_norm_squared(np.dot(inputs,A) - outputs),
+                      lmbd* tracenorm(A)])
+
         gradient = np.dot(utu,A) - uv
         C = A - stepsize * gradient
         U, s, V = np.linalg.svd(C)
-        s = s - lmbd*np.ones(np.shape(s)[0])
+
+        debugging = i==1 or i == 500
+        if debugging:
+            print "i = "+str(i)
+            print "SVD decomposition:"
+            print U
+            print s
+            print V
+        s = s - (lmbd/2)*np.ones(np.shape(s)[0])
+        if debugging:
+            print "S minus lambda"
+            print s
+
         sz = np.array([s, np.zeros(np.shape(s)[0])])
+        if debugging:
+            print "sz"
+            print sz
+
         final_s = sz.max(0)
+        if debugging:
+            print "final_s"
+            print final_s
+
         lu = np.shape(U)[1]
         lv = np.shape(V)[0]
         S = np.zeros((lu, lv), dtype=complex)
         rk = min(lu, lv)
         S[:rk,:rk] = np.diag(final_s)
+        if debugging:
+            print "in a matrix"
+            print S
+            print "\n"
         A = np.dot(U, np.dot(S, V))
 
-    return A
-
-def tracenorm(A):
-    U, s, V = np.linalg.svd(A)
-    return sum(s)
+    return A, costs
 
 
--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import regression
 
 inputs = np.array([ [1,1,4], [2,3,0], [4,3,1], [3,2,9], [4.5,3.9,2], [5.1,4,6], [6,7,1] ])
 outputs = np.array( [[5, 1.5], [4, 1.5], [3.5, 3], [13, 3], [6, 4], [10.5, 4], [7, 6.5] ])
 
-lmbd = 0.001
+lmbd = 1
+iterations = 1000
 
 def print_stats(res):
     print "Cost: "+str(regression.fitness(inputs, outputs, res))
@@ -12,21 +14,39 @@ def print_stats(res):
     print "Trace norm: "+str(regression.tracenorm(res))
 
 print "Result with closed-form ridge regression:"
-res = regression.ridge(inputs, outputs, lmbd)
+res, cost = regression.ridge(inputs, outputs, lmbd)
 print res
 print_stats(res)
+
+bestL2Cost = cost[0]+cost[1]
+upperBoundTNCost = cost[0]+lmbd*regression.tracenorm(res)
 
 print ""
 
 print "Result with gradient descent ridge regression:"
-res = regression.incremental_ridge(inputs, outputs, lmbd, 1000, 0.001)
+res, costIR = regression.incremental_ridge(inputs, outputs, lmbd, iterations, 0.001)
 print res
 print_stats(res)
+
+for i in range(len(costIR)):
+    total_cost = costIR[i][0] + costIR[i][1]
+    costIR[i].append(total_cost)
+    costIR[i].append(bestL2Cost)
 
 print ""
 
 print "Result with gradient descent trace norm regression:"
-res = regression.incremental_tracenorm(inputs, outputs, lmbd, 1000, 0.001)
+res, costTN = regression.incremental_tracenorm(inputs, outputs, lmbd, iterations, 0.001)
 print res
 print_stats(res)
+
+for i in range(len(costTN)):
+    total_cost = costTN[i][0] + costTN[i][1]
+    costTN[i].append(total_cost)
+    costTN[i].append(upperBoundTNCost)
+
+plt.plot(costIR)
+plt.show()
+plt.plot(costTN)
+plt.show()
 
